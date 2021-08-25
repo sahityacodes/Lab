415dada2b84fcf07d2f879c42f28e129809b448a
sahityacodes <sahityareddyftw@gmail.com>
2021-08-25 12:54:22 +0200

    changed intergated security to false

--- DALayer/Utils/Constants.cs
+++ DALayer/Utils/Constants.cs
@@ -2,7 +2,7 @@
 {
     sealed class Constants
     {
-        public static string ConnectionString = @"Data Source=np:\\.\pipe\LOCALDB#5F910DDA\tsql\query;Initial Catalog=CustomerDB;Integrated Security=True;";
+        public static string ConnectionString = @"Data Source=np:\\.\pipe\LOCALDB#3A209CE2\tsql\query;Database=CustomerDB;User Id=sa;Password=1234qwer;Integrated Security=false";
         public static string DBNull = ""; 
     }
 }
