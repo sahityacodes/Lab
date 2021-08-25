74992d5b77806f350a2aa90b66c5a3022e5df7b2
sahityacodes <sahityareddyftw@gmail.com>
2021-08-25 11:27:43 +0200

    removed Data tools

--- DALayer/Utils/Constants.cs
+++ DALayer/Utils/Constants.cs
@@ -2,7 +2,7 @@
 {
     sealed class Constants
     {
-        public static string ConnectionString = @"Data Source=np:\\.\pipe\LOCALDB#C6BD02C8\tsql\query;Initial Catalog=CustomerDB;Integrated Security=True;";
+        public static string ConnectionString = @"Data Source=np:\\.\pipe\LOCALDB#5F910DDA\tsql\query;Initial Catalog=CustomerDB;Integrated Security=True;";
         public static string DBNull = ""; 
     }
 }
